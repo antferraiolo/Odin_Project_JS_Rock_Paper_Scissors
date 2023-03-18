cedde823e4c50f37204166bbb251c05eeab80ab1
antferraiolo <54289964+antferraiolo@users.noreply.github.com>
2023-03-18 14:41:00 -0400

    Working on getting player choice
    Got the computer choice function working
    Now working on getting the input from the
    user. Then will move into playing a round
    of rock paper and scissors

---
+++ script.js
@@ -0,0 +1,22 @@
+function getComputerChoice(){
+    let options = ["Rock", "Paper", "Scissors"];
+    
+    //Selects number for array
+    let compselectionindex = Math.floor(Math.random() * 3);
+    let compselection = options[compselectionindex];
+    return compselection;
+}
+
+function getPlayerChoice{
+    //Get Player Choice through textbox
+
+    // Convert Player choice into either "Rock", "Paper", or "Scissors"
+
+    // Confirm Choice is Rock, Paper, or Scissors
+
+    // If not, reask for Player Choice, if correct store and return selection
+}
+
+function playRound(playerSelection,computerSelection){
+    // CODE
+}
