acd5ace907255d14fde5c4f2b8a22b6d667cba6b
antferraiolo <54289964+antferraiolo@users.noreply.github.com>
2023-03-19 15:12:41 -0400

    Play a game to 5 function works
    Will begin some more customization possibly, so win to 7 based on user input. Really though now I want to focus on moving to clicking rock, paper and scissors as an option vs saying it.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let opt1 = "Rock";
 let opt2 = "Paper";
 let opt3 = "Scissors";
+let wins_by_player = 0; 
+let wins_by_computer = 0;
+
 
 function getComputerChoice(){
     let options = [opt1, opt2, opt3];
@@ -29,7 +32,7 @@ function playRound(playerSelection,computerSelection){
     // CODE
     computerSelection = getComputerChoice();
     playerSelection = getPlayerChoice();
-    console.log("Computer Selects " + computerSelection + "!")
+    console.log("Computer Selects " + computerSelection + "!");
 
     switch (true){
         //Case with player selecting rock
@@ -38,31 +41,53 @@ function playRound(playerSelection,computerSelection){
             break;
         case ((computerSelection == opt3 && playerSelection == opt1)):
             console.log("Player Wins!");
+            wins_by_player++;
             break;
         case ((computerSelection == opt2 && playerSelection == opt1)):
             console.log("Computer Wins!");
+            wins_by_computer++;
             break;
         //Cases with player selecting scissors
         case ((computerSelection == opt1 && playerSelection == opt3)):
             console.log("Computer Wins!");
+            wins_by_computer++;
             break;
         case ((computerSelection == opt3 && playerSelection == opt3)):
             console.log("Tie!");
             break;
         case ((computerSelection == opt2 && playerSelection == opt3)):
             console.log("Player Wins!");
+            wins_by_player++;
             break;
         //Cases with player selecting paper
         case ((computerSelection == opt1 && playerSelection == opt2)):
             console.log("Player Wins!");
+            wins_by_player++;
             break;
         case ((computerSelection == opt3 && playerSelection == opt2)):
             console.log("Computer Wins!");
+            wins_by_computer++;
             break;
         case ((computerSelection == opt2 && playerSelection == opt2)):
             console.log("Tie!");
             break;
 
     }
+    console.log("Wins by player: " + wins_by_player);
+    console.log("Wins by computer: " + wins_by_computer);
+}
+
+function game(){
+    // Want to call the play round function
+    // Until either computer or player reaches 5 wins
+    while ( wins_by_computer < 5 && wins_by_player < 5){
+        playRound();
+        if(wins_by_computer == 5){
+            console.log("Computer wins the game!")
+        }
+        else{
+            console.log("Player wins the game!")
+        }
 
     }
+}
